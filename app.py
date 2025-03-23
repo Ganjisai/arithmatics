6bffc5dfbdda6e58808c53dc9349fed224e56027
Ganjisai <penchalasaiganji@gmail.com>
2025-03-23 10:03:02 +0530

    Your commit message

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 import streamlit as st
-st.title('arithmatic operations')
 
-num1=st.number_input('enter a number')
-num2=st.number_input('enter another number')
-operation=st.selectbox('operation',('add','subtract','multiply','divide'))
-if operation=='add':
-    st.write("the sum is")
-    st.write(num1+num2)
-elif operation=='subtract':
-    st.write("the difference is")
-    st.write(num1-num2)
-elif operation=='multiply':
-    st.write("the product is:")
-    st.write(num1*num2)
-elif operation=='divide':
-    st.write("the division is:")
-    st.write(num1/num2)
+st.title('Arithmetic Operations')
+
+num1 = st.number_input('Enter a number', format="%.2f")
+num2 = st.number_input('Enter another number', format="%.2f")
+operation = st.selectbox('Operation', ('Add', 'Subtract', 'Multiply', 'Divide'))
+
+if st.button("Calculate"):
+    if operation == 'Add':
+        st.write("The sum is:", num1 + num2)
+    elif operation == 'Subtract':
+        st.write("The difference is:", num1 - num2)
+    elif operation == 'Multiply':
+        st.write("The product is:", num1 * num2)
+    elif operation == 'Divide':
+        if num2 == 0:
+            st.error("Division by zero is not allowed!")
+        else:
+            st.write("The division result is:", num1 / num2)
+if st.button("Continue"):
+    st.experimental_rerun()
